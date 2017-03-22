f63e89fe55b4d9cebb836c45cddc2711a5f81e08
shingo45endo <shingo45endo@gmail.com>
2017-03-23 06:56:26 +0900

    Replace the default SoundPlayer with a new VoicePlayer

--- content_script.js
+++ content_script.js
@@ -49,18 +49,71 @@ let xhrResponses = {};
 // Dictation
 {
 	let contents = null;
-	let settings = {apps: {effect_volume: 1.0}};
+	let settings = {
+		apps: {
+			content_volume: 1.0,
+			effect_volume:  1.0,
+		},
+	};
 
 	// Waits for displaying the dictation quiz screen.
 	// TODO: Optimize the timing of the prepation for dictation.
 	const divObserver = new MutationObserver((records) => {
 		records.filter((record) => (record.type === 'attributes' && record.attributeName === 'class')).forEach((record) => {
 			if (record.target.classList.contains('current_screen')) {
+				SoundPlayer.mute();
 				prepareForDictation();
+			} else {
+				SoundPlayer.unmute();
 			}
 		});
 	});
 	divObserver.observe(document.getElementById('dictation_quiz_screen'), {attributes: true});
+
+	const SoundPlayer = (() => {
+		const player = document.getElementById('sound_player');
+
+		return {
+			mute: () => {
+				// setVolume() in sound_player.swf ignores falsy value. (it doesn't accept 0)
+				// And the actual volume (v: [0.0, 1.0]) is caliculated as below:
+				//   v = Math.round(volume) / 10;
+				// Therefore, to mute it, needs to set volume in the interval (0, 0.5).
+				player.setVolume(0.4);
+			},
+			unmute: () => {
+				const contentVolume = (settings.apps) ? (settings.apps.content_volume || 0.0) : 1.0;
+				player.setVolume(10 * contentVolume);
+			},
+		};
+	})();
+
+	const VoicePlayer = (() => {
+		const id = `__better_iknow_audio_${Date.now()}__`;
+		document.querySelector('#dictation_quiz_screen').insertAdjacentHTML('beforeend', `<div style="position: absolute; overflow: hidden; width: 620px; height: 32px; left: 0; right: 0; bottom: 16px; border-radius: 5px; margin: 0 auto; opacity: 0.667;"><audio controls id="${id}" style="width: 100%;"></audio></div>`);
+		const audio = document.getElementById(id);
+
+		return {
+			setSource: (url) => {
+				audio.src = url;
+				audio.load();
+			},
+			serVolume: (volume) => {
+				audio.volume = volume;
+			},
+			play: () => {
+				audio.currentTime = 0.0;
+				audio.play();
+			},
+			rewind: (sec) => {
+				audio.currentTime -= sec;
+				audio.play();
+			},
+			forward: (sec) => {
+				audio.currentTime += sec;
+			},
+		};
+	})();
 
 	/**
 	 *	Prepares the internal states for dictation.
@@ -82,6 +135,12 @@ let xhrResponses = {};
 		}
 		if (!contents) {
 			console.log('ERROR: Cannot make contents data')
+		}
+
+		//
+		const index = getCurrentSetenceIndex();
+		if (index >= 0) {
+			VoicePlayer.setSource(contents[index].soundUrl);
 		}
 
 		// Gets settings data from the stored XHR data.
@@ -134,8 +193,8 @@ let xhrResponses = {};
 
 		return () => {
 			audio.pause();
-			if (settings.apps && settings.apps.effect_volume) {
-				audio.volume = settings.apps.effect_volume;
+			if (settings.apps) {
+				audio.volume = settings.apps.effect_volume || 0.0;
 			}
 			audio.play();
 		};
@@ -191,57 +250,84 @@ let xhrResponses = {};
 	/**
 	 *	Handles key events and stops its propagation to the Dictation app if necessary.
 	 */
-	window.addEventListener('keydown', (event) => {
-		if (!isTypingMode()) {
-			return;
-		}
+	window.addEventListener('keydown', (() => {
+		const keyHandlers = {
+			' ': () => {
+				VoicePlayer.play();
+			},
+			'ArrowLeft': () => {
+				VoicePlayer.rewind(1.0);
+			},
+			'ArrowRight': () => {
+				VoicePlayer.forward(1.0);
+			},
+			'Backspace': () => {
+				// TODO: implement
+			},
+			'Enter': () => {},
+		};
 
-		if (['Enter', 'Backspace', 'ArrowLeft', 'ArrowRight'].indexOf(event.key) !== -1) {
-			event.preventDefault();
-			event.stopPropagation();
-			return;
-		}
-
-		if (!/^[a-zA-Z]$/.test(event.key)) {
-			return;
-		}
-
-		if (!contents || contents.length === 0) {
-			console.log('ERROR: Cannot get contents');
-			return;
-		}
-
-		const index = getCurrentSetenceIndex();
-		if (index < 0) {
-			console.log('ERROR: Cannot get index');
-			return;
-		}
-
-		const cursorPos = getCurrentCursorPos();
-		if (cursorPos < 0) {
-			console.log('ERROR: Cannot get cursorPos');
-			return;
-		}
-
-		const sentence = contents[index].sentence;
-		if (event.key.toLowerCase() !== sentence.charAt(cursorPos).toLowerCase()) {
-			playIncorrect();
-			displayIncorrect(event.key);
-
-			event.preventDefault();
-			event.stopPropagation();
-			return;
-
-		} else {
-			displayIncorrect('');
-		}
-
-		setTimeout(() => {
-			if (isSentenceCompleted()) {
-				clickEnter();
+		function handleSpecialKeys(event) {
+			if (keyHandlers[event.key]) {
+				keyHandlers[event.key]();
+				return true;
+			} else {
+				return false;
 			}
-		}, 100);
-	}, true);
+		}
+
+		return (event) => {
+			if (!isTypingMode()) {
+				return;
+			}
+
+			if (handleSpecialKeys(event)) {
+				event.preventDefault();
+				event.stopPropagation();
+				return;
+			}
+
+			if (!/^[a-zA-Z]$/.test(event.key)) {
+				return;
+			}
+
+			if (!contents || contents.length === 0) {
+				console.log('ERROR: Cannot get contents');
+				return;
+			}
+
+			const index = getCurrentSetenceIndex();
+			if (index < 0) {
+				console.log('ERROR: Cannot get index');
+				return;
+			}
+
+			const cursorPos = getCurrentCursorPos();
+			if (cursorPos < 0) {
+				console.log('ERROR: Cannot get cursorPos');
+				return;
+			}
+
+			const sentence = contents[index].sentence;
+			if (event.key.toLowerCase() !== sentence.charAt(cursorPos).toLowerCase()) {
+				playIncorrect();
+				displayIncorrect(event.key);
+
+				event.preventDefault();
+				event.stopPropagation();
+				return;
+
+			} else {
+				displayIncorrect('');
+			}
+
+			setTimeout(() => {
+				if (isSentenceCompleted()) {
+					clickEnter();
+				}
+			}, 100);
+		};
+	})(), true);
 
 	/**
 	 *	Clicks the "Enter" button in the Dictation app.
